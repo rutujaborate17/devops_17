2335aa57547aaeb180bf963b3814ea61d3d686e1
rutujaborate17 <rutujaborate17@gmail.com>
2024-10-23 17:59:36 +0530

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,1 +1,9 @@
-
+resource "aws_instance" "vm" {
+    tags = {
+      Name ="server"
+    }
+    ami ="ami-06b21ccaeff8cd686"
+    instance_type = "t2.micro"
+    key_name = "u"
+    availability_zone = "us-east-1a"  
+}
