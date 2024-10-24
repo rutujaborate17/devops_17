ab019598ce3bc09685690b7197e54e0daed2cd75
rutujaborate17 <rutujaborate17@gmail.com>
2024-10-24 18:18:37 +0530

    Update main.tf

--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -1,24 +1,16 @@
-module "vpc" {
-    source = "./VPC"
-    vpc_cidr_block = var.vpc_cidr_block
-    subnet_cidr_block = var.subnet_cidr_block
-    az = var.az
-    public_ip = var.public_ip
-    port_no = var.port_no
-    tags = var.tags
 
- 
+module "EC2" {
+    source = "./EC2"
+    ami = var.ami
+    instance_type = var.instance_type
+    key = var.key
 }
 
 
-
-
-module "ec2" {
-    source = "./EC2"
-    ami_id = var.ami_id
-    instance_type = var.instance_type
-    az = var.az
-    key = var.key
-    tags = var.tags
-
+module "vpc" {
+    source = "./vpc"
+    vpc_cidr_block = var.vpc_cidr_block
+    subnet_cidr_block = var.subnet_cidr_block
+    az=var.az
+    port_no = var.port_no
 }
