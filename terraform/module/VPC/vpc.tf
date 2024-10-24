9bc6a2a6b48388f8e66a9318744e97b3a7f7a7dc
rutujaborate17 <rutujaborate17@gmail.com>
2024-10-24 18:17:25 +0530

    Update vpc.tf

--- terraform/module/VPC/vpc.tf
+++ terraform/module/VPC/vpc.tf
@@ -1,60 +1,63 @@
-
-# VPC resource
 resource "aws_vpc" "net" {
-  cidr_block = var.vpc_cidr_block
-  tags = var.tags
+    cidr_block = var.vpc_cidr_block
+    tags = {
+      Name ="vpc_1"
+    }
 }
 
-# Subnet resource
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.net.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.az[0]
-  map_public_ip_on_launch = true
-  tags = merge(var.tags, { Name = "Public-Subnet" })
+    vpc_id = aws_vpc.net.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone =var.az[1]
+    map_public_ip_on_launch = "true"
+    tags = {
+      Name ="public_subnet"
+    }
 }
 
-# Internet Gateway
+
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.net.id
-  tags = merge(var.tags, { Name = "vpc-internet-gateway" })
+    vpc_id = aws_vpc.net.id
+    tags = {
+      Name ="internet_gateway"
+    }
+  
 }
 
-# Route Table
-resource "aws_route_table" "rt-pub" {
-  vpc_id = aws_vpc.net.id
-  tags = merge(var.tags, { Name = "RT-Public" })
+resource "aws_route_table" "rt" {
+    vpc_id = aws_vpc.net.id
+    tags = {
+      Name ="route_table"
+    }
 
-  route {
-    gateway_id = aws_internet_gateway.igw.id
-    cidr_block = "0.0.0.0/0"
-  }
+
+    route {
+        gateway_id = aws_internet_gateway.igw.id
+        cidr_block ="0.0.0.0/0"
+    }  
 }
 
-#Route Table Association
-resource "aws_route_table_association" "rta" {
-  subnet_id = aws_subnet.public.id
-  route_table_id = aws_route_table.rt-pub.id
-}
-
-#Security Group
 resource "aws_security_group" "sg" {
-  name = "firewall-vnet"
-  vpc_id = aws_vpc.net.id
+    vpc_id = aws_vpc.net.id
+    tags = {
+      Name ="security_group"
+    }
 
-  ingress {
-    from_port = var.port_no[1]
-    to_port = var.port_no[1]
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        from_port = var.port_no[1]
+        to_port = var.port_no[1]
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port = var.port_no[0]
-    to_port = var.port_no[0]
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  tags = merge(var.tags, { Name = "Security-Group" })
+    egress {
+        from_port = var.port_no[0]
+        to_port = var.port_no[0]
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+        
+    }
+  
 }
+
